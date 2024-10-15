183d9d94adfd7b1cac0d00f6af5d029ad4664b96
ZARMAIN-AHMED <nazzshagufta604@gmail.com>
2024-10-15 19:43:24 +0500

    assignment01

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const MyName = () => {
+  return (
+   <div>
+    <h1 className='bg-red-400 h-auto w-60 text-center mt-52  ml-80 font-semibold text-4xl rounded-lg p-4'>HELLO WORLD!</h1><br/>
+    <h1 className=' bg-amber-700 h-64 w-60 text-center ml-80 font-extrabold text-4xl rounded-lg p-4'>WELCOME TO NEXT.JS WORLD</h1>
+   </div>
+  );
+}
+
+export default MyName;
+
+
